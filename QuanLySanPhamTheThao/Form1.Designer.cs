7422ae62ffb17f9fd62d418755b8619d229fd370
DuyQuang <quangnkt1976@gmail.com>
2023-01-10 21:28:00 +0700

    push

--- QuanLySanPhamTheThao/Form1.Designer.cs
+++ QuanLySanPhamTheThao/Form1.Designer.cs
@@ -80,9 +80,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Location = new System.Drawing.Point(416, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.Size = new System.Drawing.Size(342, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Quản lý sản phẩm";
             // 
@@ -123,7 +125,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(456, 469);
+            this.button3.Location = new System.Drawing.Point(540, 472);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(102, 33);
             this.button3.TabIndex = 24;
